103720d9974bd137c1baf106f99ef68a3080739d
yuxian <andyretajeff@gmail.com>
2019-04-19 23:40:50 +0800

    modify to needed

--- main.cpp
+++ main.cpp
@@ -6,15 +6,6 @@
 #include <stdio.h>
 using namespace std;
 
-/*
-流程：
-    V1. 讀檔
-    V2. 建立image array
-    V3. 用bfs建立connected array 並計算數量
-    V4. 根據selected pixel找到該connected component並將裡面所有點換成目標color 
-    5. output
-*/
-
 struct triple
 {
     int row;
@@ -23,13 +14,12 @@ struct triple
     triple(int row, int col, char c):row(row),col(col),c(c){}
 };
 
-
-int main(){
-    string image_file_location = "./sample.in";
-    string output_file_location = "./test.out";
-    int target_row = 1;
-    int target_col = 1;
-    char t_color = '4';
+int main(int argc, char* argv[]){
+    string image_file_location = argv[1];
+    string output_file_location = argv[5];
+    int target_row = atoi(argv[3])+1;
+    int target_col = atoi(argv[2])+1;
+    char t_color = *argv[4];
 
     int component_count = 1;
     int row_col[2];
@@ -52,9 +42,9 @@ int main(){
         size_t len = 0;
         getline(&line, &len, image_file);
         char* pch = strtok (line," ");
-        int i=0;
+        int i=1;
         while (pch != NULL && i<2){
-            row_col[i++] = atoi(pch);
+            row_col[i--] = atoi(pch);
             pch = strtok (NULL, " ");
         }
     }
@@ -78,6 +68,24 @@ int main(){
         } 
     }
     fclose(image_file);
+
+// find target pixel and change the color
+    queue<struct triple> q;
+    q.push( triple(target_row, target_col, image_ary[target_row][target_col]));
+    char o_color = image_ary[target_row][target_col];
+    while(!q.empty() && t_color!=o_color){
+        struct triple now = q.front();
+        q.pop();
+        image_ary[now.row][now.col] = t_color;
+        for(int x=0; x<4; x++){
+            int n_r = now.row+dir[x][0];
+            int n_c = now.col+dir[x][1];
+            if( image_ary[n_r][n_c] == o_color ){
+                q.push( triple(n_r, n_c, now.c));
+            }
+        }
+    }
+
 
 // make connected array and component_count the number of connected component
     int conn_ary[row+2][col+2];
@@ -111,46 +119,30 @@ int main(){
         }
     }
 
-// find target pixel and change the color
-    queue<struct triple> q;
-    int this_com = conn_ary[target_row][target_col];
-    q.push( triple(target_row, target_col, image_ary[target_row][target_col]));
-    while(!q.empty()){
-        struct triple now = q.front();
-        q.pop();
-        image_ary[now.row][now.col] = t_color;
-        for(int x=0; x<4; x++){
-            int n_r = now.row+dir[x][0];
-            int n_c = now.col+dir[x][1];
-            if( conn_ary[n_r][n_c] == this_com && image_ary[n_r][n_c]!=t_color){
-                q.push( triple(n_r, n_c, now.c));
-            }
-        }
-    }
-
 // write output
     FILE *output = fopen(output_file_location.c_str(),"wb");
     char blank = ' ';
     char nextL = '\n';
     char firstL[2];
-    sprintf(firstL,"%d", row_col[0]);
+    sprintf(firstL,"%d", row_col[1]);
     fwrite(&firstL, 1, 1, output);
     fwrite(&blank, 1, 1, output);
-    sprintf(firstL,"%d", row_col[1]);
+    sprintf(firstL,"%d", row_col[0]);
     fwrite(&firstL, 1, 1, output);
     fwrite(&nextL, 1, 1, output);
     for(int i=1; i<row+1; i++){
         for(int j=1; j<col+1; j++){
             char c = image_ary[i][j];
             fwrite(&c, 1, 1, output);
-            fwrite(&blank, 1, 1, output);
+            if(j!=col) fwrite(&blank, 1, 1, output);
         }
         fwrite(&nextL, 1, 1, output);
     }
     int digit = log10(component_count)+1;
     char lastL[digit];
-    sprintf(lastL, "%d", component_count);
+    sprintf(lastL, "%d", component_count-1);
     fwrite(&lastL, 1, digit, output);
+    fwrite(&nextL, 1, 1, output);
 
 
     return 0;
